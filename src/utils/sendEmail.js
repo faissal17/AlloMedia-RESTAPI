2be9f58a01a6455281bb01a0200e901729230a1c
faissal17 <faissalaoukacha@gmail.com>
2023-10-19 14:03:24 +0100

    send email with generated token to reset password

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,27 +1,31 @@
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
 
 const sendEmail = async (email) => {
+  const token = jwt.sign({ email }, process.env.SECRET_KEY, {
+    expiresIn: 600,
+  });
   try {
     const transport = nodemailer.createTransport({
       host: process.env.HOST,
       port: 2525,
       auth: {
-        user: "c404e95b4d8947",
-        pass: "cb3f96913c7600",
+        user: process.env.USER,
+        pass: process.env.PASSWORD,
       },
     });
     await transport.sendMail({
       from: "AlloMedia@gmail.com",
       to: email,
       subject: "Reset Password",
-      text: "hiiiiiiiiiiiiiii",
+      html: `<h1>Click this link to reset your password</h1><a href="http://localhost:3000/api/auth/resetpassword/:${token}">Reset Password Link</a>`,
     });
     console.log("email send successufully");
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
 };
 module.exports = sendEmail;
